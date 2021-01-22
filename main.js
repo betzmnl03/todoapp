c486a70d0bcb71e977d9ba174b3a27969d94dbec
betzmnl03 <70501653+betzmnl03@users.noreply.github.com>
2021-01-22 14:02:03 -0800

    updated main.js

--- main.js
+++ main.js
@@ -5,9 +5,6 @@ $(document).ready(() => {
     let count=0;
 
  $('.btn').on('click',()=>{
-    
-    
-   
     count+=1;
     let tasknum="task"+count;
     let taskcl="."+tasknum;
@@ -15,20 +12,18 @@ $(document).ready(() => {
     let check=$("<i class='fa fa-check'></i>");
     let task=$(".item").append($(`<div class=${tasknum}></div>`).text($('.form-control').val()));
     let final=$(taskcl).append(del,check);
-    
+    let closesttext=".taskcl.border"
     final.addClass("border");
-    $('.form-control').val("")
-
-     
-    $('i.fa.fa-trash').on('click', (event)=>{
-        $(event.currentTarget).parent().remove();
-       })
-
-
-    $('i.fa.fa-check').on('click', (event)=>{
-        $(event.currentTarget).parent().toggleClass("strike");
-       })
     
+   
+      })
+
+
+      $('.item').on('click', 'i.fa.fa-trash', (event)=>{
+        $(event.currentTarget).parent().remove();
+      })
+      $('.item').on('click', 'i.fa.fa-check', (event)=>{
+        $(event.currentTarget).parent().toggleClass("strike");
       })
 
  
